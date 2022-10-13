038b0a4198d660ca421c3184901874884c7b51f1
zetta-YanChris-Difa <chris.difa@zettabyte.life>
2022-10-13 12:53:16 +0800

    Fix volume and song progress bar to be using input type range

--- src/app/core/shared/components/audio-player/audio-player.component.ts
+++ src/app/core/shared/components/audio-player/audio-player.component.ts
@@ -8,6 +8,7 @@ import {
   ChangeDetectorRef,
   OnDestroy,
 } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { distinctUntilChanged, of } from 'rxjs';
 import { GenresService } from 'src/app/core/services/genres.service';
 import { SongsService } from 'src/app/core/services/songs.service';
@@ -21,12 +22,20 @@ import { SubSink } from 'subsink';
 export class AudioPlayerComponent implements OnInit, AfterViewInit, OnDestroy {
   currentPlayingSong: any = null;
   @ViewChild('audioPlayer') audioPlayer: ElementRef;
-  @ViewChild('songProgressContainer') songProgressContainer: ElementRef;
+  @ViewChild('songRange') songRange: ElementRef;
+  @ViewChild('volumeRange') volumeRange: ElementRef;
   private subs = new SubSink();
   currentPlayingSongDuration: string;
-  songProgress: string = '0%';
+  songProgress: number = 0;
   songCurrentTime: string = '0:00';
   currentPlayingGenre: any;
+  songCtrl: FormControl<number> = new FormControl(0);
+  volumeCtrl: FormControl<number> = new FormControl(0);
+
+  playBackMode = {
+    repeatOne: false,
+    shuffle: false,
+  };
 
   constructor(
     private songsService: SongsService,
@@ -40,7 +49,38 @@ export class AudioPlayerComponent implements OnInit, AfterViewInit, OnDestroy {
     this.getCurrentPlayingSong();
     this.getCurrentTime();
     this.getCurrentPlayingGenre();
+    this.subsFormControlsVal();
     this.cdr.detectChanges();
+  }
+
+  subsFormControlsVal() {
+    this.subs.sink = this.volumeCtrl.valueChanges
+      .pipe(distinctUntilChanged())
+      .subscribe((resp) => {
+        if (this.audioPlayer?.nativeElement) {
+          this.audioPlayer.nativeElement.volume = resp / 100;
+          this.volumeRange.nativeElement.style.backgroundSize =
+            ((resp - 0) * 100) / (100 - 0) + '% 100%';
+        }
+      });
+    this.volumeCtrl.setValue(80);
+
+    this.subs.sink = this.songCtrl.valueChanges.subscribe((resp) => {
+      if (this.audioPlayer?.nativeElement?.currentTime) {
+        const songDuration: number = this.audioPlayer.nativeElement.duration;
+        const songClickedDuration: number = (songDuration * resp) / 100;
+        this.audioPlayer.nativeElement.currentTime = songClickedDuration;
+      }
+    });
+    this.songCtrl.setValue(0);
+  }
+
+  toggleVolume() {
+    if (this.volumeCtrl.value) {
+      this.volumeCtrl.setValue(0);
+    } else {
+      this.volumeCtrl.setValue(80);
+    }
   }
 
   getCurrentPlayingSong(): void {
@@ -117,8 +157,7 @@ export class AudioPlayerComponent implements OnInit, AfterViewInit, OnDestroy {
       this.audioPlayer.nativeElement.ontimeupdate = () => {
         this.songProgress =
           (this.audioPlayer.nativeElement.currentTime * 100) /
-            this.audioPlayer.nativeElement.duration +
-          '%';
+          this.audioPlayer.nativeElement.duration;
         this.songCurrentTime = this.timeFormat(
           this.audioPlayer.nativeElement.currentTime
         );
@@ -126,7 +165,17 @@ export class AudioPlayerComponent implements OnInit, AfterViewInit, OnDestroy {
           this.audioPlayer.nativeElement.currentTime ===
           this.audioPlayer.nativeElement.duration
         ) {
-          this.songsService.setCurrentPlayingSong(this.currentPlayingSong);
+          const temp = Object.keys(this.playBackMode).filter(
+            (key) => this.playBackMode[key]
+          );
+          if (!temp?.length) {
+            this.songsService.setCurrentPlayingSong(this.currentPlayingSong);
+          } else {
+            if (temp[0] === 'repeatOne') {
+              this.songsService.setCurrentPlayingSong(this.currentPlayingSong);
+              this.songsService.setCurrentPlayingSong(this.currentPlayingSong);
+            }
+          }
         }
       };
     }
@@ -148,18 +197,14 @@ export class AudioPlayerComponent implements OnInit, AfterViewInit, OnDestroy {
     });
   }
 
-  goToDuration(event: PointerEvent) {
-    const progressWidthClicked: number =
-      event.clientX -
-      this.songProgressContainer.nativeElement.getBoundingClientRect().left;
-    const fullProgressWidth: number =
-      this.songProgressContainer.nativeElement.clientWidth;
-    const songDuration: number = this.audioPlayer.nativeElement.duration;
-    const percentageProgressWidth: number =
-      (progressWidthClicked / fullProgressWidth) * 100;
-    const songClickedDuration: number =
-      (songDuration * percentageProgressWidth) / 100;
-    this.audioPlayer.nativeElement.currentTime = songClickedDuration;
+  setPlayBackMode(type: string) {
+    if (type === 'repeatOne') {
+      this.playBackMode.repeatOne = this.playBackMode.repeatOne ? false : true;
+      this.playBackMode.shuffle = false;
+    } else if (type === 'shuffle') {
+      this.playBackMode.shuffle = this.playBackMode.shuffle ? false : true;
+      this.playBackMode.repeatOne = false;
+    }
   }
 
   ngOnDestroy(): void {
